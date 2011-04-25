5b1ca5b967b241166e904fcdb4ef4853f4554010
Colin Harrington <colin.harrington@gmail.com>
2011-04-24 21:43:32 -0500

    updated Tracker to just track the consolidated.db file

--- src/nl/redcode/iphone/Tracker.java
+++ src/nl/redcode/iphone/Tracker.java
@@ -21,25 +21,25 @@ public class Tracker {
 	public static void main(String[] args) throws Exception {
 		if(args == null || args.length != 1) {
 			System.out.println("Usage: ");
-			System.out.println("java -jar iPhoneJTrack.jar \"<location of backup directory>\"");
+			System.out.println("java -jar iPhoneJTrack.jar \"<location of consolidated.db file>\"");
 			System.out.println("");
 			System.out.println("For example:");
-			System.out.println("java -jar iPhoneJTrack.jar \"C:\\Users\\roy\\AppData\\Roaming\\Apple Computer\\MobileSync\\Backup\\b4d1c79c2f5fc1da044c18d1066b80b39b575590\"");
+			System.out.println("java -jar iPhoneJTrack.jar \"./consolidated.db\"");
 			System.out.println("");
 			System.out.println("The output is a file named 'iPhoneData.kml' which can be loaded into Google Earth for example.");
 			System.exit(1);
 		}
-		
-		File directory = new File(args[0].replace("\\", "/"));
-		
-		FileLister f = new FileLister(directory);
-		File sqlLite = f.getHiddenFile();
+
+		File sqlLite = new File(args[0].replace("\\", "/"));
 		System.out.println(sqlLite);
 		
 		GeoDataExtractor e = new GeoDataExtractor();
 		List<DataPoint> geoData = e.extractGeoData(sqlLite);
 		
 		KMLGenerator kmlGenerator = new KMLGenerator();
-		kmlGenerator.generateKML(new File("iPhoneData.kml"), geoData);
+		File kmlFile = new File("iPhoneData.kml");
+		kmlGenerator.generateKML(kmlFile, geoData);
+		
+		System.out.println("Generated KML file at " + kmlFile.getAbsoluteFile());
 	}
 }
